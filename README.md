511cc2357312f4db9b2134da7b2c387bc6e5c4db
km-kurisu <kamleshkmistry33@gmail.com>
2025-05-23 11:04:17 +0530

    Day 4

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portfolio
+
+## This is my first portfolio made in next.js 
+
+you can view my portfolio by clicking on [km-kurisu.github.io](http://km-kurisu.github.io)
+it is deployed on github pages
+
+moving it on a custom domain soon
---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/react-portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    type: "module"
+  };
+  
+
+  export default nextConfig;
