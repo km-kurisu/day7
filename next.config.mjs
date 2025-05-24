a2e670796f572fa976271c9777eba480d89af213
km-kurisu <kamleshkmistry33@gmail.com>
2025-05-24 14:23:37 +0530

    conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/react-portfolio",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
     type: "module"
   };
   
